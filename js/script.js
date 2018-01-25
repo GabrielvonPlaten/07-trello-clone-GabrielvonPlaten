d13da590add4d4e5c29f280e43b9fd0587004789
Gabriel von Platen <gabrielvonplaten@gmail.com>
2018-01-25 15:46:51 +0100

    Minor fixes & moved dialog code to script.js

--- js/script.js
+++ js/script.js
@@ -1,24 +1,53 @@
 $(document).ready(function () {
+    $('.add-card-dialog').dialog({ autoOpen: false });
+    $('header').on('click', '#add-card', function () { 
+        $('.add-card-dialog').dialog('open');
+    });
 
     //Makes the cards draggable
     //I still am not sure why I need 'clone' <-- ask Axel
     $('.card').draggable({helper: 'clone'});
 
     $('.__column').droppable({
-        //The columns will accept all elements with the 'card' class
         accept: '.card',
         drop: function(event, ui) {
 
-            var movedItem = $(ui.draggable).draggable();
+            let movedItem = $(ui.draggable).draggable();
             $(this).append(movedItem);
         }
     });
 
-    /* Delete cards */
-    $(".delete-card").on("click", function (e) { 
-        e.preventDefault();
-        $(this).parent().remove();
+    /* Card propeties */
+    let cardTitle = $('#card-title-form').val();
+    let cardDescription = $('#card-description').val();
+    let deleteButton = '<button class="delete-card">Delete</button>';
+
+    //Create new card
+    $('.add-card-dialog').on('click', '#add-new-card', function () {
+        //If the forms are filled, then execute
+        if($('#card-title-form').val() && $('#card-description').val()) {
+
+        $('<div/>', {
+            class: 'card',
+            html: '<h3 class="card__title">' + $('#card-title-form').val()
+            + '</h3>' + '<p class="card-description">' + 
+            $('#card-description').val() + '</p>' + deleteButton
+            }).fadeIn(200).appendTo('.todo');
+        } else {
+            alert('You must fill out the forms.');
+        }
     });
 
-    
+    //Click on card for description
+    //Bugged
+    $('.card-description').dialog({ autoOpen: false });
+    $('.body').on('click', '.card', function () { 
+        $('.card-description', this).dialog('open');
+    });
+
+    /* Delete cards */
+    $('.card').on('click', '.delete-card', function () {
+        $(this).parent().toggle('explode');
+    });
+
 });
